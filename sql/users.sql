22c9a0cb1f53cd7c4cc081d3e02699ff0b921917
Code45Life <oxr3827@gmail.com>
2025-01-04 19:18:29 +0800

    Update users.sql

--- sql/users.sql
+++ sql/users.sql
@@ -32,7 +32,7 @@ CREATE TABLE `users` (
   `username` varchar(100) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `role` enum('admin','staff','customer') NOT NULL DEFAULT 'customer'
+  `role` enum('admin','customer') NOT NULL DEFAULT 'customer'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
